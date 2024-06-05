2139eb42a4d109009b88abe5a571ce3fa513f5c1
connect2avni <smrutictc100@gmail.com>
2024-06-06 02:40:43 +0530

    Dark mode enbled

--- src/About.js
+++ src/About.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 
 export default function About() {
-    const [mode,setMode]= useState('light')
-    const toggleMode= () =>{
-        if(mode==="light"){
+    const [mode, setMode] = useState('light')
+    const toggleMode = () => {
+        if (mode === "light") {
             setMode('dark');
-            document.body.Style.backgroundColor= 'black';
+            document.body.Style.backgroundColor = 'black';
         }
-        else{
+        else {
             setMode('light');
+            document.body.Style.backgroundColor = 'white';
         }
     }
     return (
-        <div>
 
-            <div class="card">
-                <div class="card-body">
-                    This is some text within a card body.
+        <div className="card">
+            <div className="card-body">
+                This is some text within a card body.
+            </div>
+            <div className="card-footer">
+                <div className="form-check form-switch">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        role="switch"
+                        id="flexSwitchCheckChecked"
+                        defaultChecked
+                    />
+                    <label
+                        className="form-check-label"
+                        htmlFor="flexSwitchCheckChecked"
+                    >
+                        Checked switch checkbox input
+                    </label>
                 </div>
             </div>
+            <button type="button"  class="btn btn-primary">Primary</button>
         </div>
-    )
+    
+
+    );
+
 }
